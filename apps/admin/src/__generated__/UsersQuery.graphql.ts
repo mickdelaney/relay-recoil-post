86e4320b1ffb7cec00920a2a8ae57f3efff749be
mick delaney <mick@elevatedirect.com>
2022-09-10 09:24:42 +0000

    Add Company Details To UserItem

--- apps/admin/src/__generated__/UsersQuery.graphql.ts
+++ apps/admin/src/__generated__/UsersQuery.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<b8104a289a90bce63911f36a190d3022>>
+ * @generated SignedSource<<c397fead78c6ea7640ca1fc766dd60a0>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -117,6 +117,18 @@ return {
                 "kind": "ScalarField",
                 "name": "phone",
                 "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "Company",
+                "kind": "LinkedField",
+                "name": "company",
+                "plural": false,
+                "selections": [
+                  (v1/*: any*/)
+                ],
+                "storageKey": null
               }
             ],
             "storageKey": null
@@ -127,12 +139,12 @@ return {
     ]
   },
   "params": {
-    "cacheID": "84d79f778dea000aa66deede9f8ec932",
+    "cacheID": "d9c15bc8761be08ff5f639cb1cbe8e0c",
     "id": null,
     "metadata": {},
     "name": "UsersQuery",
     "operationKind": "query",
-    "text": "query UsersQuery {\n  users {\n    data {\n      id\n      name\n      ...UserItem_user\n    }\n  }\n}\n\nfragment UserItem_user on User {\n  id\n  name\n  email\n  phone\n}\n"
+    "text": "query UsersQuery {\n  users {\n    data {\n      id\n      name\n      ...UserItem_user\n    }\n  }\n}\n\nfragment UserItem_user on User {\n  id\n  name\n  email\n  phone\n  company {\n    name\n  }\n}\n"
   }
 };
 })();
